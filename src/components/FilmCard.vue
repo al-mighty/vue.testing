dee15eaec1f4ad2d11a30d47b96c7542cac28fe8
vyacheslav kovalev <dev.vyacheslav@gmail.com>
2017-11-08 00:54:52 +0300

    restructure component

---
+++ src/components/FilmCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card"><img src="http://via.placeholder.com/253x200" alt="" class="card-img-top">
+    <div class="card-body">
+      <h4 class="card-title">{{card.title}}</h4>
+      <p class="card-text">{{card.desc}}</p>
+      <p class="card-tags">
+        <span @click="onTagClicked(tag)" class="badge badge-light" :key="tag" v-for="tag in card.tags">{{tag}}</span>
+      </p>
+      <div class="card-footer">
+        <button class="btn btn-success"><span class="fa fa-thumbs-up"></span></button>
+        <button class="btn btn-danger"><span class="fa fa-thumbs-down"></span></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['card'],
+    methods: {
+      onTagClicked(tag){
+        this.$emit('tag',tag);
+      }
+    }
+  }
+</script>
---
+++ src/components/FilmFilter.vue
@@ -0,0 +1,27 @@
+<template>
+    <form action=" ">
+        <div class="form-group">
+            <input type="text" :value="value" @input="onValueUpdated($event.target.value)" class="form-control">
+        </div>
+        <p>
+            <span class="badge badge-light" v-for="tag in tags" :key="tag" @click="onTagClicked(tag)">#{{tag}} </span>
+        </p>
+    </form>
+</template>
+
+<script>
+  export default {
+    props: ['value', 'tags'],
+    methods: {
+      onValueUpdated(value){
+        this.$emit('input', value);
+      },
+      onTagClicked(tag) {
+        this.$emit('tag', tag);
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
